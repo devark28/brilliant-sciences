2c1b271e5de60cc884a8ff1b693676e0738bfe2c
devark28 <82113556+devark28@users.noreply.github.com>
2022-04-26 07:02:11 -0700

    Update ProfilePage.js

--- components/adminPages/ProfilePage.js
+++ components/adminPages/ProfilePage.js
@@ -1,5 +1,5 @@
-import {Fragment} from "react"
-import {Stack, Slider, IconButton, Button, Select, MenuItem} from "@mui/material"
+import {Fragment, useState} from "react"
+import {Stack, Typography, CircularProgress, Button, Select, MenuItem} from "@mui/material"
 import {
   PlayArrowRounded as PlayIcon,
   VolumeUpRounded as VolumeIcon,
@@ -12,6 +12,25 @@ import ImageCard from "../ImageCard"
 import SelectItem from "../settingItems/SelectItem"
 
 export default () => {
+  const username = "Teacher"
+  const email = "teacher@ovutex.com"
+
+  const [uploadingImage, setUploadingImage] = useState(false)
+  const [image, setImage] = useState("/apparatus.jpg")
+  const [theme, setTheme] = useState(1)
+  const [themeName, setThemeName] = useState("")
+  
+  const handleImageFile = (e) => {
+    if(e.target.files[0]){
+      const url = URL.createObjectURL(e.target.files[0])
+      setUploadingImage(true)
+      setTimeout(() => {
+        setUploadingImage(false)
+        setImage(url)
+      }, 5000);
+    }
+  }
+
   return (
     <Stack style={{
       flex: 1,
@@ -25,12 +44,22 @@ export default () => {
         marginRight: "1rem"
       }}>
         <ContentCard title="Profile">
-          <ImageCard style={{maxHeight: "10rem"}} src="/apparatus.jpg" content={
+          <ImageCard style={{maxHeight: "10rem"}} src={image} content={
             [
-              ["{{username}}"],
-              ["{{email or phone number}}"],
+              [username],
+              [email],
               [<Button variant="outlined" style={{height: "2rem"}}>Change Password</Button>],
-              [<Button variant="outlined" style={{height: "2rem"}}>Change Profile Picture</Button>]
+              [<Button component="label" variant="outlined" style={{height: "2rem"}}>
+                <Stack direction="row" spacing={1} alignItems="center">
+                  <Typography style={{fontSize: ".875rem"}}>Change Profile Picture</Typography>
+                  <input type="file" accept=".jpg, .png" hidden onChange={handleImageFile}/>
+                  {
+                    uploadingImage
+                    ? <CircularProgress size={18}/>
+                    : <Fragment/>
+                  }
+                </Stack>
+              </Button>]
             ]
           }/>
         </ContentCard>
@@ -38,7 +67,13 @@ export default () => {
           <SelectItem text="Theme" options={[
               "Light",
               "Dark"
-          ]}/>
+          ]}
+          value={theme}
+          onChange={(value, name) => {
+            setTheme(value)
+            setThemeName(name)
+          }}
+          />
         </ContentCard>
       </Stack>
     </Stack>
