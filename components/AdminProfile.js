4024e4b4bf2facf5f15a98e94fe85a8a2e107f3a
devark28 <82113556+devark28@users.noreply.github.com>
2022-04-26 05:36:36 -0700

    Update AdminProfile.js

--- components/AdminProfile.js
+++ components/AdminProfile.js
@@ -1,4 +1,4 @@
-import {Fragment} from "react"
+import {Fragment, useState} from "react"
 import {Stack, Button} from "@mui/material"
 import MenuButton from "./MenuButton"
 import Footer from "./Footer"
@@ -7,6 +7,7 @@ import RequestsPage from "./adminPages/RequestsPage"
 import CoursesPage from "./adminPages/CoursesPage"
 import PricingPage from "./adminPages/PricingPage"
 export default () => {
+  const [pager, setPager] = useState(1)
   return (
     <div style={{
       display: "flex",
@@ -20,11 +21,11 @@ export default () => {
         // background: "red"
         }}>
           <h3 style={{margin: ".5rem 0 1rem 0", textAlign: "center"}}>Settings</h3>
-          <MenuButton id={1} variant="">Profile</MenuButton>
-          <MenuButton id={2} variant="">Reviews</MenuButton>
-          <MenuButton id={3} helper="{{number}} requests" variant="">Requests</MenuButton>
-          <MenuButton id={4} helper="{{upload progress}}%" variant="contained">Courses</MenuButton>
-          <MenuButton id={5} variant="">Pricing</MenuButton>
+          <MenuButton key={1} id={1} onClick={(id) => {setPager(id)}} variant={pager == 1 ? "contained" : ""}>Profile</MenuButton>
+          <MenuButton key={2} id={2} onClick={(id) => {setPager(id)}} variant={pager == 2 ? "contained" : ""}>Reviews</MenuButton>
+          <MenuButton key={3} id={3} onClick={(id) => {setPager(id)}} variant={pager == 3 ? "contained" : ""} helper="{{number}} requests">Requests</MenuButton>
+          <MenuButton key={4} id={4} onClick={(id) => {setPager(id)}} variant={pager == 4 ? "contained" : ""} helper="{{upload progress}}%">Courses</MenuButton>
+          <MenuButton key={5} id={5} onClick={(id) => {setPager(id)}} variant={pager == 5 ? "contained" : ""}>Pricing</MenuButton>
           <div style={{display: 'flex', flex: 1}}></div>
           <Stack spacing={1} direction="row">
             <Button className="no-text-wrap" variant="outlined" fullWidth>Home</Button>
@@ -39,7 +40,7 @@ export default () => {
         minHeight: "100vh",
         // background: "green"
         }}>
-          {renderPage(4)}
+          {renderPage(pager)}
           {/* <Footer style={{backgroundColor: "white", margin: "2rem 0"}}/> */}
       </Stack>
     </div>
