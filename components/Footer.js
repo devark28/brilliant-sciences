be3d3ab452aa6bf899eaa1e18a7e8e76f697c57a
devark28 <82113556+devark28@users.noreply.github.com>
2022-08-28 21:47:18 +0200

    Add files via upload

--- components/Footer.js
+++ components/Footer.js
@@ -6,21 +6,25 @@ export default ({style}) => {
         "Footer",
         "flex",
         "flex-row",
-        "justify-space-between")} style={{flexWrap: "wrap", ...style}}>
-        <ul id='socials' className={classList("flex","flex-column","flex-1","flex-start")}>
+        "justify-space-between",
+        "lg:flex-row",
+        "lg:p-0-3rm",
+        "xsm:flex-col",
+        "xsm:p-0-3rm")} style={style}>
+        <ul id='socials' className={classList("flex","flex-column","flex-1","flex-start","my-[1rem]")}>
             <li>Our Socials</li>
             <li>Facebook</li>
             <li>Twitter</li>
             <li>Instagram</li>
         </ul>
-        <ul className={classList("flex","flex-column","flex-1","flex-start")}>
+        <ul className={classList("flex","flex-column","flex-1","flex-start","my-[1rem]")}>
             <li>Our Coverage</li>
             <li>Mathematics</li>
             <li>Physics</li>
             <li>Chemistry</li>
             <li>Biology</li>
         </ul>
-        <ul className={classList("flex","flex-column","flex-1","flex-start")}>
+        <ul className={classList("flex","flex-column","flex-1","flex-start","my-[1rem]")}>
             <li>Quick Links</li>
             <li><a href="/">home</a></li>
             <li><a href="/about">about</a></li>
